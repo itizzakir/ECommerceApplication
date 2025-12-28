952b4b62c70628350bd5a47233f22593f56bc97e
Md Zakir Hussain <itizzakir@gmail.com>
2025-12-28 23:45:06 +0530

    Checkout page fixed

--- ECommerceApplication Frontend/src/pages/customer/CheckoutPage.jsx
+++ ECommerceApplication Frontend/src/pages/customer/CheckoutPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../../context/CartContext';
 import { useNotification } from '../../context/NotificationContext';
+import './CheckoutPage.css';
 
 const CheckoutPage = () => {
   const { cartItems, clearCart } = useCart();
